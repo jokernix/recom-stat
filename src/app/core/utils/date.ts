dddec14cc0729a9e057c23212445184e4388ff23
Bolotnikov Nick <jokernix@mail.ru>
2020-03-12 08:43:35 +0300

    fixed calculation of norm

--- src/app/core/utils/date.ts
+++ src/app/core/utils/date.ts
@@ -6,6 +6,7 @@ import {
   isSameDay,
   isToday,
   isWeekend,
+  isWithinInterval,
   startOfDay
 } from 'date-fns';
 import { holidays, workingDays } from '../models/working-days';
@@ -15,17 +16,22 @@ const SECONDS_IN_DAY = 8 * SECONDS_IN_HOUR;
 const OFFICE_HOURS = [9, 18];
 
 function calculateNormOfWorkingTime(date: Date): number {
+  const now = new Date();
   const startOfWorkingDay = setTime(date, OFFICE_HOURS[0]);
   const endOfWorkingDay = setTime(date, OFFICE_HOURS[1]);
 
-  if (isToday(date) && isBefore(new Date(), endOfWorkingDay)) {
-    const lunchTime = setTime(date, 13);
-
-    const norm = differenceInSeconds(new Date(), startOfWorkingDay);
-    return isBefore(new Date(), lunchTime) ? norm : norm - SECONDS_IN_HOUR;
+  if (isToday(date) && isBefore(now, startOfWorkingDay)) {
+    return 0;
   }
 
-  if (isToday(date) && isAfter(new Date(), endOfWorkingDay)) {
+  if (isToday(date) && isWithinInterval(now, { start: startOfWorkingDay, end: endOfWorkingDay })) {
+    const lunchTime = setTime(date, 13);
+
+    const norm = differenceInSeconds(now, startOfWorkingDay);
+    return isBefore(now, lunchTime) ? norm : norm - SECONDS_IN_HOUR;
+  }
+
+  if (isToday(date) && isAfter(now, endOfWorkingDay)) {
     return SECONDS_IN_DAY;
   }
 
